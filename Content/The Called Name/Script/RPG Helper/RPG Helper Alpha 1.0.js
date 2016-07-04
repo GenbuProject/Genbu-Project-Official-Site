051c3e717218511b5dd8a7cb03a58df8a5b94201
GenbuHase <hasegenbu@gmail.com>
2016-07-04 16:45:00 +0900

    ?

--- Content/The Called Name/Script/RPG Helper/RPG Helper Alpha 1.0.js
+++ Content/The Called Name/Script/RPG Helper/RPG Helper Alpha 1.0.js
@@ -7,7 +7,7 @@ var RPGHelper = function () {
 	var Style = document.createElement("Link");
 		document.head.appendChild(Style);
 		Style.setAttribute("Rel", "StyleSheet");
-		Style.setAttribute("HREF", "Script/RPG Helper/RPG Helper Alpha 1.0.css");
+		Style.setAttribute("HREF", "Script/RPG Helper/RPG Helper Alpha 1.0.js");
 		
 	this.R = {
 		SPEED: {
