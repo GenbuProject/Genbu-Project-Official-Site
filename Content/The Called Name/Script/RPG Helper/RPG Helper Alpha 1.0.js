d48234173f5041ae1f30306f897308291e92054b
Genbu Hase <hasegenbu@gmail.com>
2016-07-08 22:14:29 +0900

    Update RPG Helper Alpha 1.0.js

--- Content/The Called Name/Script/RPG Helper/RPG Helper Alpha 1.0.js
+++ Content/The Called Name/Script/RPG Helper/RPG Helper Alpha 1.0.js
@@ -112,7 +112,40 @@ var RPGHelper = function () {
 					Dialog.style.left = this.Canvas.style.width.split("px")[0] - (this.Canvas.style.width.split("px")[0] / 10) * (10 - Size[0].substr(1, 1)) + "px";
 				}
 				
-				this.Canvas.appendChild(Dialog);
+			this.Canvas.appendChild(Dialog);
+		},
+		
+		MenuItem: function (Size, ClickFuc) {
+			var Dialog = document.createElement("RPGHelper-Menu-MenuItem");
+				Dialog.style.position = "Absolute";
+				
+				if (Size[0].substr(0, 1) != "^" && Size[1].substr(0, 1) != "^" && Size[0].substr(1, 1) != "^" && Size[1].substr(1, 1) != "^") {
+					Dialog.style.width = (this.Canvas.style.width.split("px")[0] / 10) * (Math.max(Size[0].substr(1, 1), Size[1].substr(1, 1)) - Math.min(Size[0].substr(1, 1), Size[1].substr(1, 1))) - 5 + "px";
+					Dialog.style.height = (this.Canvas.style.height.split("px")[0] / 10) * (Math.max(Size[0].substr(0, 1), Size[1].substr(0, 1)) - Math.min(Size[0].substr(0, 1), Size[1].substr(0, 1))) - 5 + "px";
+					Dialog.style.top = (this.Canvas.style.height.split("px")[0] / 10) * (Math.min(Size[0].substr(0, 1), Size[1].substr(0, 1))) + "px";
+					Dialog.style.left = (this.Canvas.style.width.split("px")[0] / 10) * (Math.min(Size[0].substr(1, 1), Size[1].substr(1, 1))) + "px";
+				} else if (Size[1].substr(0, 1) == "^" && Size[1].substr(1, 1) != "^") {
+					Dialog.style.width = (this.Canvas.style.width.split("px")[0] / 10) * (Math.max(Size[0].substr(1, 1), Size[1].substr(1, 1)) - Math.min(Size[0].substr(1, 1), Size[1].substr(1, 1))) - 5 + "px";
+					Dialog.style.height = (this.Canvas.style.height.split("px")[0] / 10) * (10 - Size[0].substr(0, 1)) - 10 + "px";
+					Dialog.style.top = this.Canvas.style.height.split("px")[0] - (this.Canvas.style.height.split("px")[0] / 10) * (10 - Size[0].substr(0, 1)) + "px";
+					Dialog.style.left = (this.Canvas.style.width.split("px")[0] / 10) * (Math.min(Size[0].substr(1, 1), Size[1].substr(1, 1))) + "px";
+				} else if (Size[1].substr(0, 1) != "^" && Size[1].substr(1, 1) == "^") {
+					Dialog.style.width = (this.Canvas.style.width.split("px")[0] / 10) * (10 - Size[0].substr(1, 1)) - 5 + "px";
+					Dialog.style.height = (this.Canvas.style.height.split("px")[0] / 10) * (Math.max(Size[0].substr(0, 1), Size[1].substr(0, 1)) - Math.min(Size[0].substr(0, 1), Size[1].substr(0, 1))) - 10 + "px";
+					Dialog.style.top = (this.Canvas.style.height.split("px")[0] / 10) * (Math.min(Size[0].substr(0, 1), Size[1].substr(0, 1))) + "px";
+					Dialog.style.left = this.Canvas.style.width.split("px")[0] - (this.Canvas.style.width.split("px")[0] / 10) * (10 - Size[0].substr(1, 1)) + "px";
+				} else if (Size[1].substr(0, 1) == "^" && Size[1].substr(1, 1) == "^") {
+					Dialog.style.width = (this.Canvas.style.width.split("px")[0] / 10) * (10 - Size[0].substr(1, 1)) - 10 + "px";
+					Dialog.style.height = (this.Canvas.style.height.split("px")[0] / 10) * (10 - Size[0].substr(0, 1)) - 10 + "px";
+					Dialog.style.top = this.Canvas.style.height.split("px")[0] - (this.Canvas.style.height.split("px")[0] / 10) * (10 - Size[0].substr(0, 1)) + "px";
+					Dialog.style.left = this.Canvas.style.width.split("px")[0] - (this.Canvas.style.width.split("px")[0] / 10) * (10 - Size[0].substr(1, 1)) + "px";
+				}
+				
+				Dialog.onclick = function () {
+					ClickFuc();
+				}
+				
+			this.Canvas.appendChild(Dialog);
 		}
 	}
 	
