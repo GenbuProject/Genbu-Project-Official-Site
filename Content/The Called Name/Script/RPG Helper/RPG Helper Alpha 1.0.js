f7b7286130a6e941c0c274bf7a89d2c7ef941fbe
Genbu Hase <hasegenbu@gmail.com>
2016-07-10 11:32:04 +0900

    Update RPG Helper Alpha 1.0.js

--- Content/The Called Name/Script/RPG Helper/RPG Helper Alpha 1.0.js
+++ Content/The Called Name/Script/RPG Helper/RPG Helper Alpha 1.0.js
@@ -210,8 +210,8 @@ var RPGHelper = function () {
 				
 			ParentPanel.appendChild(Dialog);
 			
-			if (Dialog.style.width.split("px")[0] + Dialog.parentElement.style.left.split("px")[0] >= this.Canvas.style.width.split("px")[0] - 5) {
-				Dialog.style.width = Dialog.style.width.split("px")[0] - 5 + "px";
+			if (parseInt(Dialog.style.width.split("px")[0]) + parseInt(Dialog.parentElement.style.left.split("px")[0]) >= parseInt(this.Canvas.style.width.split("px")[0] - 5)) {
+				Dialog.style.width = parseInt(Dialog.style.width.split("px")[0]) - 5 + "px";
 			}
 			
 			return Dialog;
