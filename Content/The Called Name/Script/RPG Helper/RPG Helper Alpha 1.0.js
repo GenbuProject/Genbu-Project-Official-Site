8fc916a808e995df5f39935eb608f280afcc6bfe
Genbu Hase <hasegenbu@gmail.com>
2016-07-12 10:39:35 +0900

    Update RPG Helper Alpha 1.0.js

--- Content/The Called Name/Script/RPG Helper/RPG Helper Alpha 1.0.js
+++ Content/The Called Name/Script/RPG Helper/RPG Helper Alpha 1.0.js
@@ -53,7 +53,7 @@ var RPGHelper = function () {
 					
 					Reader.onload = function () {
 						this.Resource = JSON.parse(Reader.result);
-						console.log(JSON.parse(Reader.result));
+						console.log(this.Resource);
 					}
 			});
 			
