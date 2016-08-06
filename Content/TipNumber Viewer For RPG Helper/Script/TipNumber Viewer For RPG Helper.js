9112bcbc7fa1eaec3232fe7ad6d9c960729ecbeb
Genbu Hase <hasegenbu@gmail.com>
2016-08-06 13:00:23 +0900

    Update TipNumber Viewer For RPG Helper.js

--- Content/TipNumber Viewer For RPG Helper/Script/TipNumber Viewer For RPG Helper.js
+++ Content/TipNumber Viewer For RPG Helper/Script/TipNumber Viewer For RPG Helper.js
@@ -23,6 +23,15 @@ function View(File) {
 					var Ctx = Cvs.getContext("2d");
 						Ctx.clearRect(0, 0, Img.width, Img.height);
 						Ctx.drawImage(Img, 0, 0);
+						
+					for (var y = 0; y < Img.height / 16; y++) {
+						for (var x = 0; x < Img.width / 16; x++) {
+							Ctx.font = '8px'
+							Ctx.fillStyle = "Red";
+							
+							Ctx.fillText(x + y * 8, x * 16, (y + 1) * 16);
+						}
+					}
 				}
 		}
 }
