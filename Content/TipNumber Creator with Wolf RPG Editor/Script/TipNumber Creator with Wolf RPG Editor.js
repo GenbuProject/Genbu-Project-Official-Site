ce2e0a304058be2adc0df99a82ace245e7534afb
Genbu Hase <hasegenbu@gmail.com>
2016-08-07 12:55:03 +0900

    Update TipNumber Creator with Wolf RPG Editor.js

--- Content/TipNumber Creator with Wolf RPG Editor/Script/TipNumber Creator with Wolf RPG Editor.js
+++ Content/TipNumber Creator with Wolf RPG Editor/Script/TipNumber Creator with Wolf RPG Editor.js
@@ -1,3 +1,68 @@
+var TipData = null;
+var TipCtx = null;
+var MapData = null;
+var MapCtx = null;
+
+function Load(File) {
+	var Reader = new FileReader();
+		Reader.readAsArrayBuffer(File);
+		
+		Reader.onload = function () {
+			var Link = URL.createObjectURL(
+				new Blob(
+					[Reader.result],
+					{type: "image/png"}
+				)
+			);
+			
+			var Img = new Image();
+				Img.src = Link;
+				
+				Img.onload = function () {
+					console.log(Img);
+					
+					var Cvs = document.getElementById("TipCanvas");
+						Cvs.width = Img.width;
+						Cvs.height = Img.height;
+						
+					var Ctx = Cvs.getContext("2d");
+						Ctx.clearRect(0, 0, Img.width, Img.height);
+						Ctx.drawImage(Img, 0, 0);
+						
+					TipData = Ctx.getImageData(0, 0, Img.width, Img.height);
+					TipCvs = Ctx;
+				}
+		}
+}
+
 function Create(File) {
-	
+	var Reader = new FileReader();
+		Reader.readAsArrayBuffer(File);
+		
+		Reader.onload = function () {
+			var Link = URL.createObjectURL(
+				new Blob(
+					[Reader.result],
+					{type: "image/png"}
+				)
+			);
+			
+			var Img = new Image();
+				Img.src = Link;
+				
+				Img.onload = function () {
+					console.log(Img);
+					
+					var Cvs = document.getElementById("MapCanvas");
+						Cvs.width = Img.width;
+						Cvs.height = Img.height;
+						
+					var Ctx = Cvs.getContext("2d");
+						Ctx.clearRect(0, 0, Img.width, Img.height);
+						Ctx.drawImage(Img, 0, 0);
+						
+					MapData = Ctx.getImageData(0, 0, Img.width, Img.height);
+					MapCtx = Ctx;
+				}
+		}
 }
