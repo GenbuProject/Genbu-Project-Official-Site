5a4c3cfc78917fac7ae9dbdbbfc2fd30c70c7afe
Genbu Hase <hasegenbu@gmail.com>
2016-08-07 14:51:35 +0900

    Update TipNumber Creator with Wolf RPG Editor.js

--- Content/TipNumber Creator with Wolf RPG Editor/Script/TipNumber Creator with Wolf RPG Editor.js
+++ Content/TipNumber Creator with Wolf RPG Editor/Script/TipNumber Creator with Wolf RPG Editor.js
@@ -33,7 +33,7 @@ function Load(File) {
 						var X = i % ((Img.width / 16));
 						var Y = Math.floor(i / (Img.width / 16));
 						
-						TipDatas[i] = Ctx.getImageData(X * 16, Y * 16, 16, 16);
+						TipDatas[i] = Ctx.getImageData(X * 16, Y * 16, 16, 16).data;
 					}
 					
 					TipCvs = Ctx;
@@ -71,7 +71,7 @@ function Create(File) {
 						var X = i % ((Img.width / 32));
 						var Y = Math.floor(i / (Img.width / 32));
 						
-						MapDatas[i] = Ctx.getImageData(X * 32, Y * 32, 32, 32);
+						MapDatas[i] = Ctx.getImageData(X * 32, Y * 32, 32, 32).data;
 					}
 					
 					MapCtx = Ctx;
