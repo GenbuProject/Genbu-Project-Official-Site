2978d879e0eba16facc6a66851824013ffc7acb3
GenbuHase <hasegenbu@gmail.com>
2016-08-19 12:09:41 +0900

    test

--- Content/Kasutera Library/Script/Kasutera Library.js
+++ Content/Kasutera Library/Script/Kasutera Library.js
@@ -1,13 +1,28 @@
+//珠照：109100500854083629580
+//プログラマーGenboo：106666684430101995501
+
+var Library = [];
+
 function Update() {
 	var Token = "";
 	
 	while (true) {
 		var Reader = new XMLHttpRequest();
-			Reader.open("GET", "https://www.googleapis.com/plus/v1/people/109100500854083629580/activities/public?key=AIzaSyAmcraDAZp5kah2o3oCnH8HCnhUVbQwZ2g&maxResults=100&pageToken=" + Token, false);
+			Reader.open("GET", "https://www.googleapis.com/plus/v1/people/106666684430101995501/activities/public?key=AIzaSyAmcraDAZp5kah2o3oCnH8HCnhUVbQwZ2g&maxResults=100&pageToken=" + Token, false);
 			Reader.send(null);
 			
 		var Data = JSON.parse(Reader.responseText);
 		
+		for (var i = 0; i < Data.items.length; i++) {
+			if (Data.items[i].title.match("#珠照語録") != -1) {
+				var Mem = Data.items[i].title.replace("#珠照語録 \n", "");
+				
+				Library.push({
+					Reading: Mem.substr(0, Mem.match(/\n*/)),
+					Word: Mem.substr(Mem.match(/\n*/)).replace(/\n*/, "")
+				});
+			}
+		}
 		document.getElementById("Contents").textContent += JSON.stringify(Data, null, "\t") + "\n\n";
 		
 		Token = Data.nextPageToken;
