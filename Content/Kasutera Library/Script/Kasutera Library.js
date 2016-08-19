5fe8bbaa3cca4c3a083d5db860077c2a7cb677b2
GenbuHase <hasegenbu@gmail.com>
2016-08-19 13:37:32 +0900

    完成。

--- Content/Kasutera Library/Script/Kasutera Library.js
+++ Content/Kasutera Library/Script/Kasutera Library.js
@@ -1,6 +1,16 @@
 //珠照：109100500854083629580
 //プログラマーGenboo：106666684430101995501
 
+Date.prototype.getToday = function (SplitWord) {
+	var Day = new Date().toLocaleDateString().split("/");
+	var Dates = [Day[0], Day[1], Day[2]];
+	
+	if (Dates[1].length == 1) Dates[1] = "0" + Day[1];
+	if (Dates[2].length == 1) Dates[2] = "0" + Day[2];
+	
+	return Dates[0] + SplitWord + Dates[1] + SplitWord + Dates[2];
+}
+
 var Library = [];
 
 function Update() {
@@ -57,6 +67,36 @@ function Update() {
 			
 			clearInterval(Timer);
 			Dialog.parentElement.removeChild(Dialog);
+			
+			var DL = "";
+			
+			for (var i = 0; i < Library.length; i++) {
+				if (i == Library[i].length - 1) {
+					DL += Library[i].Reading + "\t" + Library[i].Word + "\t" + "顔文字";
+				} else {
+					DL += Library[i].Reading + "\t" + Library[i].Word + "\t" + "顔文字" + "\n";
+				}
+			}
+			
+			var Data = new Blob([DL], {
+				type: "Text/Plain"
+			});
+			
+			if (window.navigator.msSaveBlob) {
+				window.navigator.msSaveBlob(DL, "珠照語録 " + new Date().getToday("-") + ".txt");
+			} else {
+				Link = document.createElement("A");
+				Link.href = URL.createObjectURL(Data);
+				Link.download = "珠照語録 " + new Date().getToday("-") + ".txt";
+				Link.target = "_blank";
+				
+				var Click = document.createEvent("MouseEvents");
+					Click.initEvent("click", false, true);
+					
+				Link.dispatchEvent(Click);
+				
+				URL.revokeObjectURL(Data);
+			}
 			
 			break;
 		}
