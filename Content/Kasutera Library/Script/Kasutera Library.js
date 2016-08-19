e8faa61f5fb868f17d6183b5a6a09a59c522a36a
GenbuHase <hasegenbu@gmail.com>
2016-08-19 14:21:50 +0900

    動作してくれ...

--- Content/Kasutera Library/Script/Kasutera Library.js
+++ Content/Kasutera Library/Script/Kasutera Library.js
@@ -30,19 +30,15 @@ function Update() {
 		
 		document.body.appendChild(Dialog);
 		
-	var Timer = setInterval(function () {
-		var Content = "読み込み中...";
-		
-		Dialog.textContent += Content.substr(LoadingCount, 1);
+	var Looper = setInterval(function () {
+		Dialog.innerHTML += "出力中...".substr(LoadingCount, 1);
 		LoadingCount++;
 		
-		if (LoadingCount == Content.length - 1) {
-			Dialog.textContent = "";
+		if (LoadingCount == "出力中...".length - 1) {
 			LoadingCount = 0;
+			Dialog.innerHTML = "";
 		}
-	}, 200);
-	
-	var Looper = setInterval(function () {
+		
 		var Reader = new XMLHttpRequest();
 			Reader.open("GET", "https://www.googleapis.com/plus/v1/people/106666684430101995501/activities/public?key=AIzaSyAmcraDAZp5kah2o3oCnH8HCnhUVbQwZ2g&maxResults=100&pageToken=" + Token, false);
 			Reader.send(null);
@@ -63,10 +59,11 @@ function Update() {
 		Token = Data.nextPageToken;
 		
 		if (Token == undefined) {
+			clearInterval(Looper);
+			
 			document.getElementById("Contents").textContent = JSON.stringify(Library, null, "\t");
 			document.getElementById("Accept").disabled = null;
 			
-			clearInterval(Timer);
 			Dialog.parentElement.removeChild(Dialog);
 			
 			var DL = "";
@@ -98,8 +95,6 @@ function Update() {
 				
 				URL.revokeObjectURL(Data);
 			}
-			
-			clearInterval(Looper);
 		}
 	}, 50);
 }
