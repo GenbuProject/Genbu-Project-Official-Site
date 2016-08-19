270cc076307dda08444fad48f3d7ec631e5116de
GenbuHase <hasegenbu@gmail.com>
2016-08-19 13:16:33 +0900

    ok

--- Content/Kasutera Library/Script/Kasutera Library.js
+++ Content/Kasutera Library/Script/Kasutera Library.js
@@ -4,10 +4,19 @@
 var Library = [];
 
 function Update() {
+	var Token = "";
 	Library = [];
 	
-	var Token = "";
+	document.getElementById("Accept").disabled = "disabled";
 	
+	var Dialog = document.createElement("Dialog");
+		Dialog.style.left = (window.innerWidth / 4) + "px";
+		Dialog.style.top = (window.innerHeight / 4) + "px";
+		Dialog.style.width = (window.innerWidth / 2) + "px";
+		Dialog.style.height = (window.innerHeight / 2) + "px";
+		
+		document.body.appendChild(Dialog);
+		
 	while (true) {
 		var Reader = new XMLHttpRequest();
 			Reader.open("GET", "https://www.googleapis.com/plus/v1/people/106666684430101995501/activities/public?key=AIzaSyAmcraDAZp5kah2o3oCnH8HCnhUVbQwZ2g&maxResults=100&pageToken=" + Token, false);
@@ -30,6 +39,10 @@ function Update() {
 		
 		if (Token == undefined) {
 			document.getElementById("Contents").textContent = JSON.stringify(Library, null, "\t");
+			document.getElementById("Accept").disabled = null;
+			
+			Dialog.parentElement.removeChild(Dialog);
+			
 			break;
 		}
 	}
