51113ae101deac27f26799cd456f2bd576c0b260
GenbuHase <hasegenbu@gmail.com>
2016-09-10 14:41:00 +0900

    OK

--- Content/The Called Name/MonsterGraphic/エンリコ.js
+++ Content/The Called Name/MonsterGraphic/エンリコ.js
@@ -1,12 +1,15 @@
-return function (Victim) {
-	var ID = 2;
-	var Percent = Math.floor(Math.random() * 100 + 1); //1～100
-	
-	if (1 <= Percent && Percent >= 85) {
-		var Damage = Calc.GetFinalATKDamage([Calc.TypeID.Monster, ID], [Calc.TypeID.Character, 0]);
+(function () {
+	return function () {
+		var ID = 2;
+		var Percent = Math.floor(Math.random() * 100 + 1); //1～100
 		
-		MsgBox.call(R.THIS, R.POS.BOTTOM, R.SPEED.NORMAL, R.COLOR.WHITE, Resource.SystemData.Monster[ID].Name + "の攻撃！\n" + Resource.SystemData.Monster[ID].Name + "は" + Victim.Name + "に" + Damage + "のダメージを与えた！", function () {
+		if (1 <= Percent && Percent >= 85) {
+			var Damage = Calc.GetFinalATKDamage([Calc.TypeID.Monster, ID], [Calc.TypeID.Character, 0]);
+			Resource.UserData.Character[0].HP -= Damage;
 			
-		});
+			MsgBox.call(R.THIS, R.POS.BOTTOM, R.SPEED.NORMAL, R.COLOR.WHITE, Resource.SystemData.Monster[ID].Name + "の攻撃！\n" + Resource.SystemData.Monster[ID].Name + "は" + Resource.UserData.Character[0].Name + "に" + Damage + "のダメージを与えた！", function () {
+				return;
+			});
+		}
 	}
-}
+})();
