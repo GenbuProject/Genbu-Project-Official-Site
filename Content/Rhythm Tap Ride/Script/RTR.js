a04335b8471658e48fae5a0177b9e7c7717f21c0
GenbuHase <hasegenbu@gmail.com>
2016-10-29 17:23:59 +0900

    OK

--- Content/Rhythm Tap Ride/Script/RTR.js
+++ Content/Rhythm Tap Ride/Script/RTR.js
@@ -1,24 +1,10 @@
+//アクセストークン：ec4a5f7617aaa8d3ce0d26e94c4bd83909c3dc4c
 var RTR = function () {
 	RTR_this = this;
 	
 	this.Song = [];
 	
 	this.System = {
-		LoginToGithub: function () {
-			var Request = location.search.substr(1).split("&");
-			var Querys = {};
-			
-			for (var i = 0; i < Request.length; i++) {
-				Querys[Request[i].split("=")[0].toUpperCase()] = Request[i].split("=")[1];
-			}
-			
-			if (Querys.CODE) {
-				var TokenGetter = new XMLHttpRequest();
-					TokenGetter.open("POST", "https://github.com/login/oauth/access_token?client_id=9949bdc61a863614cd97&client_secret=14ded7dad578046486e41df9401824c70404ce0a&code=" + Querys.CODE, false);
-					TokenGetter.send(null);
-			}
-		},
-		
 		Load: function (OnLoad) {
 			var SongGetter = new XMLHttpRequest();
 				SongGetter.open("GET", "", true);
@@ -49,6 +35,5 @@ var RTR = function () {
 
 function Init() {
 	with (new RTR()) {
-		System.LoginToGithub();
 	}
 }
