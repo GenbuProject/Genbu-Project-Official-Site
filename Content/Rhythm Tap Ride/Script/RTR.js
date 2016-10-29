e8f53de0df91a273d3401ae201b18de65bc9e505
GenbuHase <hasegenbu@gmail.com>
2016-10-29 14:37:13 +0900

    OK

--- Content/Rhythm Tap Ride/Script/RTR.js
+++ Content/Rhythm Tap Ride/Script/RTR.js
@@ -1,13 +1,25 @@
 var RTR = function () {
 	RTR_this = this;
+	
 	this.Song = [];
 	
 	this.System = {
+		LoginToGithub: function () {
+			var Request = location.search.substr(1).split("&");
+			var Querys = {};
+			
+			for (var i = 0; i < Request.length; i++) {
+				Querys[Request[i].split("=")[0].toUpperCase()] = Request[i].split("=")[1];
+			}
+			
+			if (Querys.CODE) {
+				var TokenGetter = new XMLHttpRequest();
+					TokenGetter.open("GET", "https://github.com/login/oauth/access_token?code=" + Querys.CODE + "&client_id=9949bdc61a863614cd97&client_secret=8ee6a6c35d9e544060516035f06f670323d00912", false);
+					TokenGetter.send(null);
+			}
+		},
+		
 		Load: function (OnLoad) {
-			var TokenGetter = new XMLHttpRequest();
-				TokenGetter.open("GET", "https://github.com/login/oauth/access_token?" + location.search.split("?")[0] + "&client_id=9949bdc61a863614cd97&client_secret=8ee6a6c35d9e544060516035f06f670323d00912", false);
-				TokenGetter.send(null);
-				
 			var SongGetter = new XMLHttpRequest();
 				SongGetter.open("GET", "", true);
 				
@@ -36,5 +48,7 @@ var RTR = function () {
 }
 
 function Init() {
-	
+	with (new RTR()) {
+		System.LoginToGithub();
+	}
 }
