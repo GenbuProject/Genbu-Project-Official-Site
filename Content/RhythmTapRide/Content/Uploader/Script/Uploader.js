182aea743f1fdf64ca652b6ba0ca7e5c9892051f
GenbuHase <hasegenbu@gmail.com>
2016-11-23 12:50:54 +0900

    OK

--- Content/RhythmTapRide/Content/Uploader/Script/Uploader.js
+++ Content/RhythmTapRide/Content/Uploader/Script/Uploader.js
@@ -1,6 +1,9 @@
 //アクセストークン：atob("YWUzY2I0YTU0ZDdkMTJiMDMzODRiODk2YThiOWZlZGZhMGIwMTZiMw==")
 var Token = "";
 
+const ID = "568561761665-fgnn7jvnf1rt5pb8r275o8uagkjfusjf.apps.googleusercontent.com";
+const Secret = atob("Z05EREFjdDdYQXZHMW9iR3Y1NFZtanRu");
+
 var Util = {
 	CreateDialog: function (Title, Content, FooterContent) {
 		var DialogBack = document.createElement("Div");
@@ -51,7 +54,7 @@ var Util = {
 
 var Net = {
 	LoginWithGoogle: function () {
-		window.open("https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=https://genbuproject.github.io/Genbu-Project-Official-Site/Content/RhythmTapRide/Content/Uploader/&scope=https://www.googleapis.com/auth/plus.login+https://www.googleapis.com/auth/plus.me&response_type=code&client_id=568561761665-fgnn7jvnf1rt5pb8r275o8uagkjfusjf.apps.googleusercontent.com&key=AIzaSyDdyecB-0e1qMwYDd46w4p5Iki-TVf3_HM&access_type=offline&approval_prompt=force");
+		window.open("https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=https://genbuproject.github.io/Genbu-Project-Official-Site/Content/RhythmTapRide/Content/Uploader/&scope=https://www.googleapis.com/auth/plus.login+https://www.googleapis.com/auth/plus.me&response_type=code&client_id=" + ID + "&key=AIzaSyDdyecB-0e1qMwYDd46w4p5Iki-TVf3_HM&access_type=offline&approval_prompt=force");
 	}
 }
 
@@ -62,7 +65,7 @@ function Init() {
 		Util.CreateDialog("Googleにログインして下さい", "Rhythm Tap Ride(RTR)のアップローダーをご利用頂くため<Br>Googleにログインして下さい。", "<Button OnClick = 'Net.LoginWithGoogle();'>Sign in with Google+</Button><Button>キャンセル</Button>");
 	} else {
 		var Getter = new XMLHttpRequest();
-			Getter.open("POST", "https://www.googleapis.com/oauth2/v4/token?client_id=568561761665-fgnn7jvnf1rt5pb8r275o8uagkjfusjf.apps.googleusercontent.com&client_secret=gNDDAct7XAvG1obGv54Vmjtn&redirect_uri=https://genbuproject.github.io/Genbu-Project-Official-Site/Content/RhythmTapRide/Content/Uploader/&access_type=offline&grant_type=authorization_code&code=" + Query.CODE, true);
+			Getter.open("POST", "https://www.googleapis.com/oauth2/v4/token?client_id=" + ID + "&client_secret=" + SecretID + "&redirect_uri=https://genbuproject.github.io/Genbu-Project-Official-Site/Content/RhythmTapRide/Content/Uploader/&access_type=offline&grant_type=authorization_code&code=" + Query.CODE, true);
 			
 			Getter.onload = function () {
 				Token = JSON.parse(Getter.response).access_token;
