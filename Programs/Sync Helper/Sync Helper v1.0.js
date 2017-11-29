5d6eb1a3503f461fd0a72c483abe4b9099f953d2
Genbu Hase <hasegenbu@gmail.com>
2017-11-29 18:06:58 +0900

    著作権名義を変更。

--- Programs/Sync Helper/Sync Helper v1.0.js
+++ Programs/Sync Helper/Sync Helper v1.0.js
@@ -1,7 +1,7 @@
 /*/
  *#=================================================================================================
  *#Sync Helper v1.0
- *#Copyright (C) 2016 Genbu Project & Genbu Hase All Rights Reversed.
+ *#Copyright (C) 2016 Genbu Project All Rights Reversed.
  *#=================================================================================================
 /*/
 var GitAPI = function (Token) {
